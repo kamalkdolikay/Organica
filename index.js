bc71c94ad2d2f18ba14bfe23df5ecb2a313e9a02
Kamal Dolikay <kamaldolikay@gmail.com>
2024-03-09 06:47:09 -0800

    CORS package

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 
@@ -8,11 +9,12 @@ const connectDB = require('./config/db');
 connectDB();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use('/api/users', require('./routes/usersRoutes'));
-app.use('/api/products', require('./routes/productsRoutes.js'))
+app.use('/api/products', require('./routes/productsRoutes'))
 
 app.listen(port, () => {
     console.log('Server started on port '+ port);
