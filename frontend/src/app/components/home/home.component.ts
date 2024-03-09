f1d3cbeacae9ccd1122405c805f422a4b46a144f
Kamal Dolikay <kamaldolikay@gmail.com>
2024-03-09 09:07:56 -0800

    toggle login/logout

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isLogin: boolean = false;
+
+  constructor(
+    private authService:AuthService,
+    private router:Router) { }
+
+  ngOnInit(): void {
+    let token = this.authService.loadToken()
+    console.log("token", token);
+    if(token) {
+      this.isLogin = true
+     } else {
+      this.isLogin = false
+     }
+  }
+
+  onLogout() {
+    this.authService.logout()
+    this.router.navigate(['/login'])
+  }
 
 }
