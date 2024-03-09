f36cd05bbfd8462c4cb1df97ec1cdd42940ee17e
Kamal Dolikay <kamaldolikay@gmail.com>
2024-03-09 07:17:16 -0800

    login api implemented

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -11,6 +12,7 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class AuthService {
+  authToken: any;
   user: any;
 
   private apiUrl = 'http://localhost:3000/api/users';
@@ -19,5 +21,17 @@ export class AuthService {
 
   register(user:any) {
     return this.http.post(this.apiUrl+'/register',user,httpOptions);
+  }
+
+  login(user:any) {
+    return this.http.post(this.apiUrl+'/login',user, httpOptions)
+    .pipe(map((res: any) => res));
+  }
+
+  storeUserData(token:any, user:any){
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user))
+    this.authToken = token
+    this.user = user
   }
 }
