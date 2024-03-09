83c3d66aa196931827dfcd9a6a4d569b1f4be345
Kamal Dolikay <kamaldolikay@gmail.com>
2024-03-09 09:02:57 -0800

    angular final build serving on node

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 
@@ -12,6 +13,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+//ot be used for angular production
+app.use(express.static(path.join(__dirname, 'public/browser')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/browser/index.html'));
+});
+//--//
 
 app.use('/api/users', require('./routes/usersRoutes'));
 app.use('/api/products', require('./routes/productsRoutes'))
