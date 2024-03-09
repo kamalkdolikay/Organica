9df91693b3f69e2cec7584bdac78074acb790005
Kamal Dolikay <kamaldolikay@gmail.com>
2024-03-09 06:51:11 -0800

    frontend register routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   },
 ];
 
