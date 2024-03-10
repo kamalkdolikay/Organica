7229589711885d5cedb387cab5ec71252091d449
Kamal Dolikay <kamaldolikay@gmail.com>
2024-03-10 06:55:54 -0700

    remove cart working

--- frontend/src/app/components/consumer/consumer.component.ts
+++ frontend/src/app/components/consumer/consumer.component.ts
@@ -21,11 +21,27 @@ export class ConsumerComponent implements OnInit {
       this.user = data;
     })
     let products = localStorage.getItem('cart');
-    if(products == null){
+    console.log("prod", products)
+    if(products == null || products.length === 0){
       this.carts = [{name: 'Add products to cart'}];
     } else {
       this.isProducts = true;
       this.carts = JSON.parse(products);
     }
+  }
+
+  remove(id:any){
+    console.log("id", id)
+    let arr = this.carts
+    arr.splice(id, 1); 
+    console.log(arr)
+    if(arr.length == 0){
+      this.isProducts = false;
+      this.carts = [{name: 'Add products to cart'}];
+      localStorage.removeItem('cart');
+    } else {
+      localStorage.setItem('cart', JSON.stringify(arr));
+    }
+    
   }
 }
